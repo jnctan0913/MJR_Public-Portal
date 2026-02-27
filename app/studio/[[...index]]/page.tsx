13e1ac000c51dfaa89aa6fa3c52481fccfdf7415
jnctan0913 <jnctan0913@gmail.com>
2026-02-27 12:26:59 +0800

    Fix static export for studio catch-all route

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -3,6 +3,11 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '@/sanity/config'
 
+export function generateStaticParams() {
+  // Required for static export with optional catch-all routes.
+  return [{}]
+}
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
